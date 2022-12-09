ec23090d5aabb2e31559ffe48542991679cd51f1
unknown <guptaamarjit935@gmail.com>
2022-12-09 19:39:03 +0530

    adding files

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = {
+    products: [
+        {id: '1',
+         name: 'Earphones',
+         price: 12690,
+         image: "https://via.placeholder.com/150",
+    },
+    {id: '2',
+         name: 'Test 6x',
+         price: 1400,
+         image: "https://via.placeholder.com/150",
+    },
+    {id: '3',
+    name: 'Glory Selfie Stick',
+    price: 175,
+    image: "https://via.placeholder.com/150",
+},
+],
+};
+
+export default data;
